35b0fc32a873ebeee31d2b192180196b838193b0
Adelola Kayode Samson <xarmzon@gmail.com>
2024-11-12 22:30:11 +0100

    feat: add foody project

--- REACT/projects/foody/src/App.jsx
+++ REACT/projects/foody/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-purple-600 bg-purple-300 w-7">
+      <h1 className="text-3xl text-green text-appRose text-purple-600 bg-purple-300 w-7">
         Welcome To Foody App
       </h1>
     </>
